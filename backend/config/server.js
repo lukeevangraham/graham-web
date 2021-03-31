e6159b4155ed2a82de0693aa46bff11476647660
Cefalu Wave <luke.evan.graham@gmail.com>
2021-03-31 00:04:26 -0700

    changing frontend port

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '0b183a802d8be836fbdd77ee66acf624'),
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd941c80b-e96e-4f35-8ff9-10cea2f96aaa'
+};
