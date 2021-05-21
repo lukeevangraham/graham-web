00d6931e3afd41ca0e98bc7d6c70f173f0508f0b
Cefalu Wave <luke.evan.graham@gmail.com>
2021-05-20 17:10:04 -0700

    working on media path

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,7 +1,8 @@
 export function getStrapiURL(path = "") {
   return `${
     // process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
-    process.env.NEXT_PUBLIC_STRAPI_API_URL || "https://admin.lukegraham.us"
+    "https://admin.lukegraham.us"
+    // process.env.NEXT_PUBLIC_STRAPI_API_URL || "https://admin.lukegraham.us"
   }${path}`;
 }
 
