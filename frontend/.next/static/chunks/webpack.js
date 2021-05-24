2b2405920f4f5a2ca15016edfdd129e1b4729949
Cefalu Wave <luke.evan.graham@gmail.com>
2021-05-23 22:21:26 -0700

    contact padding reduced

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "259ffa15c83fc17053cc";
+/******/ 	var hotCurrentHash = "e7c3f0989c1645a55077";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
