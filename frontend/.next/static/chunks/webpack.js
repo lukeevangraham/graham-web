b58fcc3fc911e69c3f3e7c130c46c7878804bc4d
Cefalu Wave <luke.evan.graham@gmail.com>
2021-05-23 22:07:22 -0700

    adding 400 fontweight

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d842faece3357d8fd232";
+/******/ 	var hotCurrentHash = "09cd930afd496215d562";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
