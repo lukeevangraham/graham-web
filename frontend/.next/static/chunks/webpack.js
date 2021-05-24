4416206731894d50dc110dfdf3e9c8f63558f369
Cefalu Wave <luke.evan.graham@gmail.com>
2021-05-23 21:51:09 -0700

    media queries

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3629b97426c037ec6e63";
+/******/ 	var hotCurrentHash = "d842faece3357d8fd232";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
